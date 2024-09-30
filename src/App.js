045a4dee679991623cd6f7940adf9ed0a6abd5bb
Konnichiwaitsnahoko <154089390+Konnichiwaitsnahoko@users.noreply.github.com>
2024-09-29 17:43:11 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App Project</div>;
+  return (
+    <div className="App">
+      Weather App Project
+      <footer>
+        This project is coded by{" "}
+        <a href="https://github.com/Konnichiwaitsnahoko">Nahoko Park</a> and
+        hosted by{" "}
+        <a href="https://master--weatheapp-nahoko.netlify.app/">Netlify</a>.
+      </footer>
+    </div>
+  );
 }
-
