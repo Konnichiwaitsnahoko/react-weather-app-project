e6bbdadd0263effaf169412530eebfb2d864d5fd
Konnichiwaitsnahoko <154089390+Konnichiwaitsnahoko@users.noreply.github.com>
2024-09-30 14:59:15 -0700

    Added bootstrap.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App Project
-      <footer>
-        This project is coded by{" "}
-        <a href="https://github.com/Konnichiwaitsnahoko">Nahoko Park</a> and
-        hosted by{" "}
-        <a href="https://master--weatheapp-nahoko.netlify.app/">Netlify</a>.
-      </footer>
+      <div className="container">
+        <h1>Weather App Project</h1>
+
+        <footer>
+          This project is coded by{" "}
+          <a href="https://github.com/Konnichiwaitsnahoko" target="_blank">
+            Nahoko Park
+          </a>{" "}
+          and is opne-sourced on{" "}
+          <a
+            href="https://github.com/Konnichiwaitsnahoko/react-weather-app-project"
+            target="_blank">
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
