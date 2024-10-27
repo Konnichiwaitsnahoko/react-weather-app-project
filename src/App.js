9243f165df61b68aa3c3b807d8f398065a3f8c61
Konnichiwaitsnahoko <154089390+Konnichiwaitsnahoko@users.noreply.github.com>
2024-10-27 12:35:43 -0700

    Changed footer

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ export default function App() {
             rel="noreferrer"
             target="_blank">
             GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://www.netlify.com/" rel="noreferrer" target="_blank">
+            Netlify
           </a>
           .
         </footer>
